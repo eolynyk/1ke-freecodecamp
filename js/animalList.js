8084edc82996e0485626f9df0645bee48a4a3d28
eolynyk <eolynyk@gmail.com>
2015-07-16 23:16:52 -0700

    first commit
    simple way to pull free code camp json data using handlebarsjs

---
+++ js/animalList.js
@@ -0,0 +1,12 @@
+// JQUERY ANIMAL TEMPLATE
+
+ var source = $("#animalTemplate").html();
+ var template = Handlebars.compile(source);
+ var data = {animals: [
+   {type: "Dog", sound: "woof"},
+   {type: "Cat", sound: "meow"},
+   {type: "Cow", sound: "moo"}
+ ]};
+ $("#animalList").html(template(data));
+
+
